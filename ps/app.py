bc418c30459b07757bca649428919ac55f377878
keung <38097602+bluegmlduf2@users.noreply.github.com>
2020-12-19 23:55:05 +0900

    20201219 테이블화면작성

--- ps/app.py
+++ ps/app.py
@@ -8,10 +8,25 @@ app=Flask(__name__)
 def daumMethod(): 
     return redirect("https://www.daum.net/") 
 
-@app.route('/') 
-def main(): 
-    return redirect(url_for('daumMethod')) #url_for('함수명'),url_for('static')
+# @app.route('/') 
+# def main(): 
+#     return redirect(url_for('daumMethod')) #url_for('함수명'),url_for('static')
+@app.route('/table')
+def index():
+    '''테이블페이지'''
+    try:
+        return render_template('table.html')
+    except Exception: 
+        #sys.exc_info()
+        print(traceback.print_exc())
+        #return render_template('error_404.html')
+    finally:
+        print('프로그램 종료(Exits application)')
+
+@app.errorhandler(404)
+def page_not_found(error): 
+    return "페이지가 없습니다. URL를 확인 하세요", 404
 
 #flask run & python app.py
 # if __name__ == '__main__': 
-#     app.run(debug=true)
+app.run(debug=True)
