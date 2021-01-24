5333ebfe4e5f98daed9a1f1b35afa9d94a7d75c7
wally_yoon <bluegmlduf2@naver.com>
2021-01-24 12:49:06 +0900

    210124 dbconnection

---
+++ ps/db.py
@@ -0,0 +1,12 @@
+class DatabaseConnection:
+    # def __init__(self):
+    #     self.db_config = {
+    #         'database': DATABASES['database'],
+    #         'user': DATABASES['user'],
+    #         'password': DATABASES['password'],
+    #         'host': DATABASES['host'],
+    #         'port': DATABASES['port'],
+    #         'charset': DATABASES['charset'],
+    #         'collation': DATABASES['collation'],
+    #     }
+    #     self.db_connection = mysql.connector.connect(**self.db_config)
