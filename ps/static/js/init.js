9be6e7f0f6893acdad84b9510d7020956747dfaf
wally_yoon <bluegmlduf2@naver.com>
2021-01-28 00:49:12 +0900

    0128 자바스크립트데이터셋

--- ps/static/js/init.js
+++ ps/static/js/init.js
@@ -1,21 +1,31 @@
 /**
  * INIT
  */
-(function init() {
-    /**메뉴중분류가져오기 */
+(() => {
+    getMenu(); //메뉴중분류가져오기
+})();
+
+/**
+ * 메뉴중분류가져오기
+ */
+function getMenu() {
     module
         .ajax("POST", "/order/menu")
         .then((result) => {
-            document.querySelectorAll("#orderMidMidMenu>ul>li").forEach((btn) => {
-                let resultJson=JSON.parse(result)
-                debugger
-                resultJson.map((val,idx)=>{
-                    //menu_cd,menu_kind
-                    btn.innerHTML=val.menu_kind
-                })
-            })
+            let resultJson = JSON.parse(result);//menu_cd,menu_kind
+
+            document
+                .querySelectorAll("#orderMidMidMenu>ul>li")
+                .forEach((btn, idx) => {
+                    btn.innerHTML = resultJson[idx].menu_kind;
+                    btn.setAttribute('data-val', resultJson[idx].menu_cd);
+                    //console.log(elem.dataset.val);
+                    //console.log(elem.getAttribute('data-val')); // 데이터셋 미지원 브라우저에서는 getAttribute()로 접근해야 함
+                    //elem.dataset.val = "1";
+                    //elem.dataset //맵형태로 모든 데이터셋반환
+                });
         })
         .catch((result) => {
             console.log(result);
         });
-})();
+}
