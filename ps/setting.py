c87f4487113bf92781b80b395c53406617c5a83c
Wally_Yoon <38097602+bluegmlduf2@users.noreply.github.com>
2021-01-30 12:21:06 +0900

    Update setting.py
    className(object) 이유

--- ps/setting.py
+++ ps/setting.py
@@ -5,7 +5,7 @@ import os
 
 # 상속할 부모파일
 
-
+#기본적으로 모든 클래스는 object를 상속받으므로 클래스명(object)를 사용할 필요가 없지만 파이선2에서는 클래스명(object)가 old-style 클래스를 생성하였기에 이렇게 사용함. 즉 (object)를 안적어도 된다.
 class InitConf(object):
     DEBUG = False
     TESTING = False
