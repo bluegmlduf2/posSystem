34c50ca86e705882e6c6c03fb55730fe38ecaca7
wally_yoon <bluegmlduf2@naver.com>
2021-02-15 23:23:51 +0900

    0215 파이선공통

---
+++ ps/common.py
@@ -0,0 +1,18 @@
+import time
+import traceback
+
+def decorate(func):
+    print ("##### 메소드 시작 => ("+func.__name__+")#####")
+    start = time.time()
+    end = time.time()
+    print ("##### 메소드 종료 #####")
+    print ("소요시간: %5f" % (end-start))
+
+#유저에러_Exception상속
+class UserError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    
+    #객체가 print함수에 호출될때 표시되는 함수
+    def __str__(self):
+        return self.msg
